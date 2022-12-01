4e6bfbfbf7c9cf164e9cddff696f9cd90e3cbd0d
hhsecond <sherin@grid.ai>
2022-12-01 18:25:15 +0530

    fixing client for torch audio

--- examples/torchaudio/client.py
+++ examples/torchaudio/client.py
@@ -1,6 +1,6 @@
 import torchaudio, requests
 
-waveform, sample_rate = torchaudio.load("../audio_file.wav")
+waveform, sample_rate = torchaudio.load("../../audio_file.wav")
 waveform =  waveform[0,:].tolist()
 waveform.append(sample_rate)
 waveform = ' '.join(str(x) for x in waveform)
