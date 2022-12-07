e2c6ef75eeefe71d4f9ada432e813023402d6256
Sherin Thomas <sherin@grid.ai>
2022-12-07 22:34:40 +0530

    Update examples/torchvision/client.py

--- examples/torchvision/client.py
+++ examples/torchvision/client.py
@@ -2,7 +2,6 @@ import base64
 import requests
 from pathlib import Path
 
-# img = Path("catimage.png").read_bytes()
 img = requests.get("https://raw.githubusercontent.com/Lightning-AI/LAI-Triton-Server-Component/main/catimage.png").content
 img = base64.b64encode(img).decode("UTF-8")
 response = requests.post("http://127.0.0.1:7777/predict", json={
