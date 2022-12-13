15b2daa1a3945360660f19ce89a581e0c72b8e7a
Sherin Thomas <sherin@lightning.ai>
2022-12-13 13:17:25 +0000

    ability to disable compatibility check

--- lightning_triton/lightning_triton.py
+++ lightning_triton/lightning_triton.py
@@ -30,6 +30,7 @@ from lightning_triton.base import ServeBase
 logger = Logger(__name__)
 
 MODEL_NAME = "lightning-triton"
+DISABLE_DRIVER_COMPATIBILITY_CHECK = os.environ.get("DISABLE_DRIVER_COMPATIBILITY_CHECK", "0") == "1"
 LIGHTNING_TRITON_BASE_IMAGE = os.getenv(
     "LIGHTNING_TRITON_BASE_IMAGE", "ghcr.io/gridai/lightning-triton:v0.22"
 )
@@ -301,7 +302,7 @@ class TritonServer(ServeBase, abc.ABC):
 
         Normally, you don't need to override this method.
         """
-        if self.device == torch.device("cuda"):
+        if self.device == torch.device("cuda") and not DISABLE_DRIVER_COMPATIBILITY_CHECK:
             self._check_nvidia_driver_compatibility()
 
         self._setup_model_repository()
